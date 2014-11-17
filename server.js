f09085bdc4d8765c080d1c83166924aa0590ed0c
khalid <khalid.rahmani.mail@gmail.com>
2014-11-17 22:22:46 +0000

    fc

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ var config = require('./config/config');
 var app = express();
 var port = process.env.PORT || 3000;
 
-// Connect to mongodb
 var connect = function () {
   var options = { server: { socketOptions: { keepAlive: 1 } } };
   mongoose.connect(config.db, options);
@@ -18,14 +17,11 @@ connect();
 mongoose.connection.on('error', console.log);
 mongoose.connection.on('disconnected', connect);
 
-
 fs.readdirSync(__dirname + '/app/models').forEach(function (file) {
   if (~file.indexOf('.js')) require(__dirname + '/app/models/' + file);
 });
 
-
 require('./config/passport')(passport, config);
-
 
 require('./config/express')(app, passport);
 
