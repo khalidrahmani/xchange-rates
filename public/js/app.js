2d8c0674a2ba1b728d0acb2b4e7f5e677ec8884b
khalid <khalid.rahmani.mail@gmail.com>
2014-11-18 22:24:38 +0000

    morris chart

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,18 @@
-new Morris.Line({
-  element: 'myfirstchart',
-  data: [
-    { year: '2008', value: 20 },
-    { year: '2009', value: 10 },
-    { year: '2010', value: 5 },
-    { year: '2011', value: 5 },
-    { year: '2012', value: 20 }
-  ],
-  xkey: 'year',
-  ykeys: ['value'],
-  labels: ['Value']
+$(function() {
+	new Morris.Line({
+	  element: 'myfirstchart',
+	  data: [
+	    { year: '2008', value: 20 },
+	    { year: '2009', value: 10 },
+	    { year: '2010', value: 5 },
+	    { year: '2011', value: 5 },
+	    { year: '2012', value: 20 }
+	  ],
+	  xkey: 'year',
+	  ykeys: ['value'],
+	  labels: ['Value']
+	});
 });
-
 var app = angular.module('exchangeRatesApp', []).config(function($interpolateProvider){
     $interpolateProvider.startSymbol('{?').endSymbol('?}');
 })
