07637b48c7b4df0bbf0763adcf02a90b17fb913a
khalid <khalid.rahmani.mail@gmail.com>
2014-12-01 16:43:30 +0000

    fc

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -1,6 +1,7 @@
 var request  = require("request"),
 	  moment   = require("moment"),
     mongoose = require('mongoose'),
+    History  = mongoose.model('History') ,
     Currency = mongoose.model('Currency');
 
 exports.index = function (req, res){  
@@ -17,6 +18,53 @@ exports.show = function (req, res){
     title: 'Exchange rates'
   });
 };
+/*
+
+exports.getData = function(req, res){
+  var from = req.query.from,
+      to   = req.query.to,
+      view = req.query.view,      
+      today = moment().format('YYYY-MM-DD');
+  params = {'1week': {date_number: '1', date_arg: 'week', period: 'daily', data_range: 'd7', xlabel: 'day'},
+            '1month':{date_number: '1', date_arg: 'month', period: 'daily', data_range: 'd30', xlabel: 'day'},
+            '1year': {date_number: '1', date_arg: 'year', period: 'weekly', data_range: 'y1', xlabel: 'month'},
+            '2years':{date_number: '2', date_arg: 'year', period: 'weekly', data_range: 'y2', xlabel: 'month'},
+            '5years':{date_number: '5', date_arg: 'year', period: 'monthly', data_range: 'y5', xlabel: 'month'}}
+  param = params[view]          
+  start_date = moment().subtract(param['date_number'], param['date_arg']).format('YYYY-MM-DD');          
+  history_url  = "http://www.oanda.com/currency/historical-rates/update?quote_currency="+from+"&end_date="+today+"&start_date="+start_date+"&period="+param['period']+"&display=absolute&rate=0&data_range="+param['data_range']+"&price=mid&view=graph&base_currency_0="+to
+    request({method: "GET", url: "http://www.currency.me.uk/remote/ER-CCCS2-AJAX.php?ConvertTo="+req.query.to+"&ConvertFrom="+req.query.from+"&amount=1"}, function (err, response, body){
+      console.log(response.body)
+      var current_rate = Math.round(response.body * 10000) / 10000;
+      var ymin = ymax = current_rate;
+      History.findOne({from: from, to: to, range: view}, function (err, result){
+        console.log(result)
+        var chart_data = []        
+        data = result.data
+        for(index in data){
+          rate = Math.round(data[index][1] * 10000) / 10000
+          ymin = (rate < ymin || ymin == 0) ? rate : ymin;
+          ymax = (rate > ymax || ymax == 0) ? rate : ymax;
+          date = moment(data[index][0]).format('YYYY-MM-DD')
+          chart_data.push({date: date, value: rate});
+        }
+        if(chart_data[0].date == today)    chart_data[0].value = current_rate
+        else chart_data.push({date: today, value: current_rate});
+        ymin = getMin(ymin);
+        ymax = getMax(ymax);      
+            res.json({
+            current_rate: current_rate,
+            chart_data: chart_data,
+            ymin: ymin,
+            ymax: ymax,
+            xlabel: param.xlabel
+          });
+        });
+  });
+}
+
+*/
+
 
 exports.getData = function(req, res){
   var from = req.query.from,
