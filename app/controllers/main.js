e8b95a2d549436658d78b9df110640f9a63b7d88
khalid <khalid.rahmani.mail@gmail.com>
2015-01-04 12:50:22 +0000

    fc

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -6,7 +6,7 @@ var request   = require("request"),
 
 exports.index = function (req, res){  
     res.render('main/index', {
-      title: 'Exchange rates'
+      title: 'Currency Converter'
     });
 };
 
@@ -33,31 +33,36 @@ exports.CurrencyRSSFeeds = function (req, res){
   Currency.find({}).sort({long_name: 1}).exec(function (err, currencies){
     res.render('main/rss', {
       currencies: currencies,
-      title: 'Exchange rates'
+      title: 'RSS Feed For All Currencies'
     });
   });  
 };
 
 exports.CurrencyRSSFeed = function (req, res){
-  from_currency = req.params.currency
-  Rate.findOne({}).sort({timestamp: -1}).exec(function(err, current_rate){ 
-    var feed = new RSS({
-        title:          'Latest Exchange Rates For '+ from_currency,
-        description:    'RSS Exchange Feed for '+from_currency,
-        copyright:      'Copyright © 2015 www.xchange-rates.com All rights reserved'
-    });
-    rates = current_rate.rates
-    for(currency in rates){
-      r = (rates[currency])/(rates[from_currency])
-      r = Math.round(r * 10000) / 10000      
-      feed.item({
-        title:  from_currency+'/'+currency,
-        description: '1 ' + from_currency+ ' = '+r+' '+currency,
-        url: 'http://www.xchange-rates.com/show?amount=1&from='+from_currency+'&to='+currency+'&view=1month',
-        date: current_rate.timestamp*1000 // any format that js Date can parse.  
-      }); 
-    } 
-    res.set('Content-Type', 'text/xml');
-    res.send(feed.xml());       
-  })
+
+  Currency.getCurrencies(function(currencies_array){
+    from_currency = req.params.currency
+    Rate.findOne({}).sort({timestamp: -1}).exec(function(err, current_rate){ 
+      var feed = new RSS({
+          title:          'Latest Exchange Rates For '+ currencies_array[from_currency],
+          description:    'RSS Exchange Feed for '+from_currency,
+          copyright:      'Copyright © 2015 www.xchange-rates.com All rights reserved'
+      });
+      rates = current_rate.rates
+      for(currency in rates){
+        r = (rates[currency])/(rates[from_currency])
+        r = Math.round(r * 10000) / 10000      
+        feed.item({
+          title:  from_currency+'/'+currency,
+          description: '1 ' + currencies_array[from_currency]+ ' = '+r+' '+currencies_array[currency],
+          url: 'http://www.xchange-rates.com/show?amount=1&from='+from_currency+'&to='+currency+'&view=1month',
+          date: current_rate.timestamp*1000 // any format that js Date can parse.  
+        }); 
+      } 
+      res.set('Content-Type', 'text/xml');
+      res.send(feed.xml());       
+    })
+  })  
 };
+
+//getCurrencyLongName()
