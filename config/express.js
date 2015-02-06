09340c9e50a396e821e88b0e0ad1420cda9cabf5
khalid <khalid.rahmani.mail@gmail.com>
2015-02-06 17:31:08 +0000

    fc

--- config/express.js
+++ config/express.js
@@ -3,7 +3,7 @@ var compression = require('compression');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var swig = require('swig');
-var favicon = require('serve-favicon');
+//var favicon = require('serve-favicon');
 var config = require('./config');
 
 var env = process.env.NODE_ENV || 'development';
@@ -15,7 +15,7 @@ module.exports = function (app) {
   }));
 
   app.use(express.static(config.root + '/public'));
-  app.use(favicon(__dirname + '/../public/favicon.ico'));
+//  app.use(favicon(__dirname + '/../public/favicon.ico'));
 
   if (env === 'development' || env === 'test') {
     swig.setDefaults({
